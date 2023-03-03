cad6ce326b427db0352b96a5ca9bf5d7981acf7b
Andrew Sharp <asharp@juniper.net>
2023-03-03 09:22:28 +0000

    Proxy fix added.

--- src/KubeLibrary/KubeLibrary.py
+++ src/KubeLibrary/KubeLibrary.py
@@ -234,6 +234,8 @@ class KubeLibrary:
         Environment variables:
         - HTTP_PROXY:
           Proxy URL
+        - HTTPS_PROXY:
+          Proxy URL
         """
         self.api_client = None
         self.cert_validation = cert_validation
@@ -255,13 +257,19 @@ class KubeLibrary:
         else:
             try:
                 config.load_kube_config(kube_config, context)
+                proxy_url = (
+                    environ.get("https_proxy", None)
+                    or environ.get("HTTPS_PROXY", None)
+                    or environ.get("http_proxy", None)
+                    or environ.get("HTTP_PROXY", None)
+                )
+                if proxy_url:
+                    client.Configuration._default.proxy = proxy_url
             except TypeError:
                 logger.error('Neither KUBECONFIG nor ~/.kube/config available.')
 
         if not self.api_client:
             self.api_client = client.ApiClient(configuration=client.Configuration().get_default_copy())
-
-        self.api_client.configuration.proxy = environ.get('http_proxy') or environ.get('HTTP_PROXY')
 
         self._add_api('v1', client.CoreV1Api)
         self._add_api('networkingv1api', client.NetworkingV1Api)
